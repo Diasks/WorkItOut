bbd3cf3dc80ae6cc33161e9875b11390090cd0f2
Astrid <astrid.sinabian@chasacademy.se>
2020-03-27 15:20:52 +0100

    Add SCSS, fonts

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
