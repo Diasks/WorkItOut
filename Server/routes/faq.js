358eb6ee83aa03408673efa777f4ba3bd4fbfbcb
Astrid <astrid.sinabian@chasacademy.se>
2020-04-21 12:40:37 +0200

    T-77: Add routes and model for FAQ

---
+++ Server/routes/faq.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const router = express.Router();
+const verifyToken = require("../middleware/VerifyToken");
+const FaqSchedule = require("../models/faq");
+
+// @route GET api/faq
+// @desc Get all faq-schedules
+// @access Public
+router.get("/", async (req, res) => {
+  try {
+    const faq = await FaqSchedule.find();
+    res.json(faq);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// @route POST api/faq
+// @desc Create new faq-schedule
+// @access Private
+router.post("/", verifyToken, async (req, res) => {
+  const faq = new FaqSchedule({
+    question: req.body.question,
+    answer: req.body.answer,
+  });
+  try {
+    const savedFaq = await faq.save();
+    res.json(savedFaq);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// @route DELETE api/faq/:faqId
+// @desc Delete specific faq-schedule
+// @access Private
+router.delete("/:faqId", verifyToken, async (req, res) => {
+  try {
+    const removedFaq = await FaqSchedule.deleteOne({
+      _id: req.params.faqId,
+    });
+    res.json(removedFaq);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+// @route PATCH api/faq/:faqId
+// @desc Update specific faq-schedule
+// @access Private
+router.patch("/:faqId", verifyToken, async (req, res) => {
+  try {
+    const updatedFaq = await FaqSchedule.updateOne(
+      { _id: req.params.faqId },
+      {
+        $set: {
+          question: req.body.question,
+          answer: req.body.answer,
+        },
+      }
+    );
+    res.json(updatedFaq);
+  } catch (err) {
+    res.json(err);
+  }
+});
+
+module.exports = router;
