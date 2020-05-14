c2ecdcbef5fafe848bffe9f9c69cdd15bdd329e9
Astrid <astrid.sinabian@chasacademy.se>
2020-05-14 15:54:07 +0200

    Fix conflicts

--- Server/models/Fitness.js
+++ Server/models/Fitness.js
@@ -1,46 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const exerciseInformationSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  sets: {
-    type: Number,
-    required: true,
-  },
-  reps: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  url: {
-    type: String,
-    trim: true,
-  },
-});
-
-const FitnessSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  bodyPartName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  exerciseInformation: [exerciseInformationSchema],
-});
-
-const FitnessSchedule = mongoose.model("fitness", FitnessSchema);
-
-module.exports = FitnessSchedule;
-=======
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
@@ -79,12 +36,12 @@ const FitnessSchema = new Schema({
   length: {
     type: Number,
     required: true,
-    trim: true
+    trim: true,
   },
   title: {
     type: String,
     required: true,
-    trim: true
+    trim: true,
   },
   exerciseInformation: [exerciseInformationSchema],
 });
@@ -92,4 +49,3 @@ const FitnessSchema = new Schema({
 const FitnessSchedule = mongoose.model("fitness", FitnessSchema);
 
 module.exports = FitnessSchedule;
->>>>>>> 2b7379a0f56dd95177979a9b2a1de867a76bdf40
