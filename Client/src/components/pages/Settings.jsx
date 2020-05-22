a02c8b6d795f5e2f86520be5348304e1a3a4b7cc
Diana Skshipek <Diana.Skshipek@chasacademy.se>
2020-05-22 15:16:48 +0200

    T-76_Create-settings-for-admin

--- Client/src/components/pages/Settings.jsx
+++ Client/src/components/pages/Settings.jsx
@@ -1,11 +1,123 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { setAlert } from "../../_actions/alertAction";
+import { registerNewPassword } from "../../_actions/userAction";
+import { connect } from "react-redux";
+import Alert from "../layout/Alert";
 
-const Settings = () => {
+const Settings = ({setAlert, registerNewPassword}) => {
+
+  let defaultValues = {
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  };
+
+  const { register, handleSubmit, errors } = useForm({ defaultValues });
+
+
+  const [passwordData, setPasswordData] = useState({
+    oldPassword: "",
+    newPassword: "",
+    confirmPassword: "",
+  });
+
+  const { oldPassword, newPassword, confirmPassword } = passwordData;
+
+  const onChange = (e) => {
+    setPasswordData({ ...passwordData, [e.target.name]: e.target.value });
+  };
+
+  const onSubmit = async (e) => {
+    if (newPassword !== confirmPassword) {
+      setAlert("Lösenord matchar inte", "danger");
+    } else {
+      registerNewPassword({ oldPassword, newPassword });
+    }
+  };
+
+
   return (
-    <main className="main">
+
+
+    <main className="main column">
       <h1>Inställningar</h1>
+      <h3>Tillåt push-notiser</h3> <button>switch me</button>
+      <p>någon text här</p>
+      <h3>Tillåt push-notiser</h3> <button>switch me</button>
+      <p>någon text här</p>
+
+      <h3>Ändra lösenord</h3>
+      <Alert />
+      <form className="form-container" onSubmit={handleSubmit((e) => onSubmit(e))}
+            noValidate>
+         <input
+              className={"input" + (errors.oldPassword ? " error border" : "")}
+              type="password"
+              name="oldPassword"
+              placeholder="Gammalt lösenord"
+              defaultValue={defaultValues.oldPassword}
+              onChange={(e) => onChange(e)}
+              ref={register({ required: true, minLength: 6 })}
+            />
+
+            {errors.oldPassword && errors.oldPassword.type === "required" && (
+              <span className="error message">Gammalt lösenord måste fyllas i</span>
+            )}
+
+
+
+
+
+
+        <input
+              className={"input" + (errors.newPassword ? " error border" : "")}
+              type="password"
+              name="newPassword"
+              placeholder="Nytt lösenord"
+              defaultValue={defaultValues.newPassword}
+              onChange={(e) => onChange(e)}
+              ref={register({ required: true, minLength: 6 })}
+            />
+
+            {errors.newPassword && errors.newPassword.type === "required" && (
+              <span className="error message">Nytt lösenord måste fyllas i</span>
+            )}
+
+            {errors.newPassword && errors.newPassword.type === "minLength" && (
+              <span className="error message">
+                Lösenord måste innehålla minst 6 tecken
+              </span>
+            )}
+
+            <input
+              className={
+                "input" + (errors.confirmPassword ? " error border" : "")
+              }
+              type="password"
+              name="confirmPassword"
+              placeholder="Repetera lösenord"
+              defaultValue={defaultValues.confirmPassword}
+              onChange={(e) => onChange(e)}
+              ref={register({ required: true })}
+            />
+
+            {errors.confirmPassword &&
+              errors.confirmPassword.type === "required" && (
+                <span className="error message">Lösenord måste fyllas i</span>
+              )}
+        <button className="btn btn-sky">Ändra</button>
+      </form>
+
+      <h3>Radera konto</h3>
+      <p>blablablabla</p>
+      <button className="btn btn-sky">Radera</button>
     </main>
   );
 };
 
-export default Settings;
+const mapStateToProps = state => ({
+  user: state.user.selectedUser,
+});
+
+export default connect(mapStateToProps, { setAlert, registerNewPassword })(Settings);
