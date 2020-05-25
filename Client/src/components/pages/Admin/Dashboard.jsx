364af596478c74e30b32c3180626d4669d9ab533
Astrid <astrid.sinabian@chasacademy.se>
2020-05-25 17:50:32 +0200

    T-106: Dashboard styling in progress

--- Client/src/components/pages/Admin/Dashboard.jsx
+++ Client/src/components/pages/Admin/Dashboard.jsx
@@ -4,57 +4,95 @@ import { Link, Redirect } from "react-router-dom";
 import { getUserProfile } from "../../../_actions/userAction";
 import { useEffect } from "react";
 import store from "../../../store";
-import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import LoadingOverlay from "react-loading-overlay";
+import PulseLoader from "react-spinners/PulseLoader";
+import UserQuantity from "./UserQuantity";
 
-const Dashboard = ({ auth: { admin }, selectedUser }) => {
+const Dashboard = ({ auth: { admin }, selectedUser, loading, users }) => {
   useEffect(() => {
     store.dispatch(getUserProfile());
   }, []);
 
-  const displayDashboard = (
-    <section>
-      <h3>
-        Hejsan{" "}
-        {selectedUser.firstname === null || undefined
-          ? ""
-          : selectedUser.firstname}
-      </h3>
+  let admins;
 
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam quos
-        enim voluptates accusantium suscipit neque fugit ex voluptatem illum!
-        Error ratione, aut iure laudantium vero expedita sapiente. Enim, nisi
-        rem.
-      </p>
-      <div>
-        {" "}
-        <Link to="/create-program">
-          Skapa nytt program{" "}
-          <ArrowForwardIosIcon className="icon icon-arrowforwardiosicon" />{" "}
-        </Link>
-      </div>
-      <div>
-        {" "}
-        <Link to="/settings">
-          Inställningar{" "}
-          <ArrowForwardIosIcon className="icon icon-arrowforwardiosicon" />{" "}
-        </Link>
-      </div>
-    </section>
+  if (users) {
+    let adminArray = users.map((user) => {
+      return user.admin;
+    });
+
+    if (adminArray) {
+      admins = adminArray.filter(function (x) {
+        return x === true;
+      }).length;
+    }
+  }
+
+  const displayDashboard = (
+    <main className="main column less-margin">
+      <section className="curved-banner">
+        <div className="banner-text">
+          <h2 className="heading mustard">
+            Hejsan, {selectedUser && selectedUser.firstname}!
+          </h2>
+          <p className="label cream">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.{" "}
+          </p>
+        </div>
+
+        <div className="curved-banner-bottom"></div>
+      </section>
+
+      <section className="box-wrapper">
+        <div className="box">
+          <h4>Antal registrerade</h4>
+          <div className="rounded-box">{users && users.length}</div>
+        </div>
+        <div className="box">
+          <h4>Antal admin</h4>
+          <div className="rounded-box">{admins}</div>
+        </div>
+      </section>
+
+      <section id="border" class="border-class">
+        <div>
+          <Link className="link-menu" to="/create-program">
+            <span>Skapa nytt program</span>
+            <span className="icon icon-arrow-right"></span>
+          </Link>
+        </div>
+        <div>
+          <Link className="link-menu" to="/settings">
+            <span>Inställningar</span>
+            <span className="icon icon-arrow-right"></span>
+          </Link>
+        </div>
+      </section>
+    </main>
   );
 
   const redirectUser = <Redirect to="/overview" />;
 
   return (
-    <main className="main">
+    <LoadingOverlay
+      active={loading}
+      spinner={<PulseLoader color={"#f5af61"} />}
+      styles={{
+        overlay: (base) => ({
+          ...base,
+          background: "#efeeee",
+        }),
+      }}
+    >
       {admin === "true" || admin === true ? displayDashboard : redirectUser}
-    </main>
+    </LoadingOverlay>
   );
 };
 
 const mapStateToProps = (state) => ({
   selectedUser: state.user.selectedUser,
   auth: state.auth,
+  loading: state.user.loading,
+  users: state.user.users,
 });
 
 export default connect(mapStateToProps, { getUserProfile })(Dashboard);
